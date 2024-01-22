d793494443047c0c0b5e428795fc8ea1d001f293
qianlv7 <qianlv7@qq.com>
2024-01-22 18:03:44 +0800

    🌈 style(project2): comment a log message

--- javascript-calisthenics/cs142-template-processor.js
+++ javascript-calisthenics/cs142-template-processor.js
@@ -5,7 +5,7 @@ function Cs142TemplateProcessor(template) {
 Cs142TemplateProcessor.prototype.fillIn = function fillIn(dictionary) {
   const pattern = /{{2}(\w+)}}/g;
   return this.template.replace(pattern, (match, p1) => {
-    console.log(match, p1);
+    // console.log(match, p1);
     if (dictionary[p1]) {
       return dictionary[p1];
     }
