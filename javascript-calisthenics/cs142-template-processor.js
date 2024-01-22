420ac86a55572dd2ac66339d040ffa3b0740d390
qianlv7 <qianlv7@qq.com>
2024-01-22 18:00:37 +0800

    ✨ feat(project2): Regular expressions

---
+++ javascript-calisthenics/cs142-template-processor.js
@@ -0,0 +1,20 @@
+function Cs142TemplateProcessor(template) {
+  this.template = template;
+}
+
+Cs142TemplateProcessor.prototype.fillIn = function fillIn(dictionary) {
+  const pattern = /{{2}(\w+)}}/g;
+  return this.template.replace(pattern, (match, p1) => {
+    console.log(match, p1);
+    if (dictionary[p1]) {
+      return dictionary[p1];
+    }
+    return "";
+  });
+}
+
+// const template = "My favorite month is {{month}} but not the day {{day}} or the year {{year}}";
+// const dateTemplate = new Cs142TemplateProcessor(template);
+// const dictionary = { month: "July", day: "1", year: "2016" };
+// const str = dateTemplate.fillIn(dictionary);
+// console.log(str);
